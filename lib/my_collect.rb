2218569918e9fc5f6c0d14e498f5913992980215
Aaron Brady <aleebrady@gmail.com>
2020-08-20 08:05:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,24 +1,4 @@
 require "pry"
 
-def my_collect(array)
-  i = 0 
-<<<<<<< HEAD
-  first_names = []
-  while i < array.length 
-  first_names << yield(array[i])
-  i = i + 1 
-end
-first_names
-end
-=======
-  first_names[]
-  while i < array.length 
-  binding.pry
-  yield(array[i])
-  i = i + 1 
-end
-end
-
->>>>>>> 0d28bfc7c9739e56433b417a7c0d806986bd5deb
 
 
